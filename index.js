8edc4764452be6cb4d8d2c1df590e77529af836c
Karl07Yammine <116973671+Karl07Yammine@users.noreply.github.com>
2025-06-26 13:15:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ import { Client, Databases, Users, Query } from "node-appwrite";
 export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_ENDPOINT)
-    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setProject(process.env.APPWRITE_PROJECT)
     .setKey(process.env.APPWRITE_API_KEY);
 
   const databases = new Databases(client);
   const users = new Users(client);
 
-  const db1 = process.env.DB1_ID;
-  const col1 = process.env.COLLECTION1_ID;
-  const db2 = process.env.DB2_ID;
-  const col2 = process.env.COLLECTION2_ID;
+  const db = process.env.APPWRITE_DB;
+  const col1 = process.env.APPWRITE.ADDRESS;
+  const col2 = process.env.APPWRITE_BOOKING;
 
   const { userId } = JSON.parse(req.payload ?? '{}');
 
@@ -25,9 +24,18 @@ export default async ({ req, res, log, error }) => {
     }
   };
 
+  const deleteDocs2 = async (db, col) => {
+    const docs = await databases.listDocuments(db, col, [
+      Query.equal("userIdentification", userId)
+    ]);
+    for (const doc of docs.documents) {
+      await databases.deleteDocument(db, col, doc.$id);
+    }
+  };
+
   try {
-    await deleteDocs(db1, col1);
-    await deleteDocs(db2, col2);
+    await deleteDocs(db, col1);
+    await deleteDocs2(db, col2);
     await users.delete(userId);
     return res.json({ success: true });
   } catch (err) {
